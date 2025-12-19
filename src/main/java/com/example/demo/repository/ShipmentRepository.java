ceada8790cf106f6be8f3343b8f35cb38411942e
bala-amypo <bala@amypo.in>
2025-12-19 04:39:07 +0000

    User commit

--- src/main/java/com/example/demo/repository/ShipmentRepository.java
+++ src/main/java/com/example/demo/repository/ShipmentRepository.java
@@ -1,6 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Shipment;
+import com.example.demo.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShipmentRepository extends JpaRepository<Shipment, Long> { }
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+    Shipment findByVehicle(Vehicle vehicle);
+}
--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,6 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Vehicle;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VehicleRepository extends JpaRepository<Vehicle, Long> { }
+import java.util.List;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+    List<Vehicle> findByUser(User user);
+
+    Vehicle findByVehicleNumber(String vehicleNumber);
+}
