b6a2972f112505c6efe28abfcfd0567997a2050d
bala-amypo <bala@amypo.in>
2025-12-26 06:15:08 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
@@ -12,9 +14,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9258.pro604cr.amypo.ai/")
-                ));
-        }
+                .info(
+                        new Info()
+                                .title("Demo API Documentation")
+                                .version("1.0")
+                                .description("Swagger documentation for Demo Project")
+                )
+                .servers(
+                        List.of(
+                                new Server()
+                                        .url("http://localhost:8080")
+                                        .description("Local Server")
+                        )
+                );
+    }
 }
